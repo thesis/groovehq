5390398dff16f47b954277bb38a84ca025d32e75
Matt Luongo <mhluongo@gmail.com>
2016-01-19 20:42:41 -0500

    Added initial setup.py.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name = 'GrooveHQ',
+    author = 'Matt Luongo',
+    version = '0.1',
+    author_email = 'mhluongo@gmail.com',
+    packages = ['groovehq'],
+    include_package_data = True,
+    install_requires = ['requests==2.9.1'],
+    license='LICENSE',
+    url = 'https://github.com/cardforcoin/groovehq',
+    keywords = 'groovehq groove api helpdesk',
+    description = 'Python API Wrapper for GrooveHQ',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet'
+    ],
+)
